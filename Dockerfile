5401be901a911301aceae953440cf10f611f5ce7
Mahdi Fooladgar <mahdi.fooladgar@yahoo.com>
2020-05-24 20:25:14 +0430

    Add nautilus support

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,14 @@ FROM ubuntu:16.04
 USER root
 WORKDIR /tmp/
 RUN apt-get update \
-    && apt-get install wget libqt4-declarative libqt4-network libqt4-network libqtgui4 -y 
+    && apt-get install -y  \
+        wget \
+        libqt4-declarative \
+        libqt4-network \
+        libqt4-network \
+        libqtgui4 \
+        xdg-utils \
+        nautilus 
 
 RUN wget http://ftp.lysator.liu.se/pub/opensuse/repositories/home:/colomboem/xUbuntu_16.04/amd64/dukto_6.0-1_amd64.deb \
     && dpkg -i dukto_6.0-1_amd64.deb 
--- dukto-docker.sh
+++ dukto-docker.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
 xhost +
-docker run --rm -d --user $UID:$GID -e USER=$USER -e USERID=$UID -e HOME=$HOME -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device /dev/dri -v $HOME:/home/$USER/ -v /etc/passwd:/etc/passwd --network host --name dukto professormahi/dukto:r6
+docker run --rm -d \
+    --user $UID:$GID -e USER=$USER -e USERID=$UID \
+    -e HOME=$HOME \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    --device /dev/dri \
+    -v $HOME:/home/$USER/ \
+    -v /etc/passwd:/etc/passwd \
+    -e DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
+    -v /run/user/$UID/bus:/run/user/$UID/bus \
+    --network host \
+    --name dukto \
+    professormahi/dukto:r6
